30b05428df41676ed25d686b93c756fd728b9863
Victor Ribeiro <victorribeiroesser@gmail.com>
2023-04-03 13:12:36 -0300

    Update meuapp.py

--- Projects/meuapp.py
+++ Projects/meuapp.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from ttkthemes import ThemedStyle
+import tkinter.ttk as ttk
 import mysql.connector as conn
 from flask import Flask, request
 
@@ -23,24 +24,29 @@ flask = Flask(__name__)
 class application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.master.geometry("460x460")
         self.master = master
         self.pack()
+        style = ThemedStyle(self.master)
+        style.set_theme("radiance")
         self.create_widgets()
         
     # Criando campos para serem preenchidos
     def create_widgets(self):
         """ Campos de preenchimento do formulário """
-        style = ThemedStyle(self.master)
-        style.set_theme("radiance")
-
-
+        
         # Campo "Nome"
-        self.name_label = tk.Label(self, font=("Arial", 11))
+        self.name_label = ttk.Label(self, font=("Arial", 11))
         self.name_label["text"] = "Nome:"
         self.name_label.pack(side="top")
 
-        self.name_entry = tk.Entry(self, font=("Arial", 8))
+        self.name_entry = ttk.Entry(self, font=("Arial", 8))
         self.name_entry.pack(side="top")
+
+        # Adiciona um espaço entre os campos
+        self.space_label = tk.Label(self, font=("Arial", 8))
+        self.space_label["text"] = ""
+        self.space_label.pack(side="top")
 
         # Campo "Sobrenome"
         self.lastname_label = tk.Label(self, font=("Arial", 11))
@@ -49,6 +55,11 @@ class application(tk.Frame):
 
         self.lastname_entry = tk.Entry(self, font=("Arial", 8))
         self.lastname_entry.pack(side="top")
+
+        # Adiciona um espaço entre os campos
+        self.space_label = tk.Label(self, font=("Arial", 8))
+        self.space_label["text"] = ""
+        self.space_label.pack(side="top")
         
         # Campo "E-mail"
         self.email_label = tk.Label(self, font=("Arial", 11))
@@ -58,9 +69,14 @@ class application(tk.Frame):
         self.email_entry = tk.Entry(self, font=("Arial", 8))
         self.email_entry.pack(side="top")
 
+        # Adiciona um espaço entre os campos
+        self.space_label = tk.Label(self, font=("Arial", 8))
+        self.space_label["text"] = ""
+        self.space_label.pack(side="top")
+
         # Botão "Enviar"
-        self.submit_button = tk.Button(self, font=("Arial", 14))
-        self.submit_button["text"] = "Enviar"
+        self.submit_button = ttk.Button(self, text="Enviar")
+        self.submit_button.configure(style='My.TButton')
         self.submit_button["command"] = self.submit
         self.submit_button.pack(side="right")
     
