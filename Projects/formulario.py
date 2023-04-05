ae5d92d18b6e6b2c3ef4f8ed82527bf57a2acbb1
Victor Ribeiro <victorribeiroesser@gmail.com>
2023-04-05 16:59:56 -0300

    Updates: Códigos gerais
    Inserido novas funções

--- Projects/formulario.py
+++ Projects/formulario.py
@@ -1,8 +1,6 @@
 import tkinter as tk
-from ttkthemes import ThemedStyle
 import tkinter.ttk as ttk
-import mysql.connector as conn
-from flask import Flask
+from ttkthemes import ThemedStyle
 from functions import conectaDb
 
 # Realiza conexão com banco de dados conforme parâmetro informado.
@@ -10,73 +8,103 @@ db = conectaDb("localhosty")
 
 sql = "INSERT INTO cadastros (nome , sobrenome, email) VALUES (%s, %s, %s)"
 
-flask = Flask(__name__)
-
-class application(tk.Frame):
+class ServiceSelection(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.master.geometry("460x460")
-        self.master = master
+        self.master.geometry("600x460")
+        self.style = ThemedStyle(self.master)
+        self.style.set_theme("radiance")
+        self.master.title("Projeto Financeiro 4.5")
         self.pack()
-        style = ThemedStyle(self.master)
-        style.set_theme("radiance")
+
         self.create_widgets()
-        
+
+    def create_widgets(self):
+        services = ["Metas", "Service 2", "Service 3"]
+
+        self.service_label = ttk.Label(self, text="Selecione um serviço:")
+        self.service_label.pack(side="left")
+
+        self.create_service_buttons(services)
+
+    def create_service_buttons(self, services):
+        for service in services:
+            button = tk.Button(self, text=service, command=lambda s=service: self.open_form(s))
+            button.pack(side="left")
+
+    def open_form(self, service):
+
+        if service == "Metas":
+            root = tk.Tk()
+            app = Application(master=root, service=service)
+            app.mainloop()
+
+class Application(tk.Frame):
+    def __init__(self, master=None, service=None):
+        super().__init__(master)
+        self.master.geometry("800x460")
+        self.master = master
+        self.master.title(f"Projeto Financeiro 4.5 - {service}")
+        self.pack()
+        self.style = ThemedStyle(self.master)
+        self.style.set_theme("radiance")
+        self.create_widgets()
+
     # Criando campos para serem preenchidos
     def create_widgets(self):
         """ Campos de preenchimento do formulário """
-        
+
         # Campo "Nome"
         self.name_label = ttk.Label(self, font=("Arial", 11))
         self.name_label["text"] = "Nome:"
-        self.name_label.pack(side="top")
+        self.name_label.pack(side="left")
 
         self.name_entry = ttk.Entry(self, font=("Arial", 8))
-        self.name_entry.pack(side="top")
+        self.name_entry.pack(side="left")
 
         # Adiciona um espaço entre os campos
         self.space_label = tk.Label(self, font=("Arial", 8))
         self.space_label["text"] = ""
-        self.space_label.pack(side="top")
+        self.space_label.pack(side="left")
 
         # Campo "Sobrenome"
         self.lastname_label = tk.Label(self, font=("Arial", 11))
         self.lastname_label["text"] = "Sobrenome:"
-        self.lastname_label.pack(side="top")
+        self.lastname_label.pack(side="left")
 
         self.lastname_entry = tk.Entry(self, font=("Arial", 8))
-        self.lastname_entry.pack(side="top")
+        self.lastname_entry.pack(side="left")
 
         # Adiciona um espaço entre os campos
         self.space_label = tk.Label(self, font=("Arial", 8))
         self.space_label["text"] = ""
-        self.space_label.pack(side="top")
-        
+        self.space_label.pack(side="left")
+
         # Campo "E-mail"
         self.email_label = tk.Label(self, font=("Arial", 11))
         self.email_label["text"] = "E-mail:"
-        self.email_label.pack(side="top")
+        self.email_label.pack(side="left")
 
         self.email_entry = tk.Entry(self, font=("Arial", 8))
-        self.email_entry.pack(side="top")
+        self.email_entry.pack(side="left")
 
         # Adiciona um espaço entre os campos
         self.space_label = tk.Label(self, font=("Arial", 8))
         self.space_label["text"] = ""
-        self.space_label.pack(side="top")
+        self.space_label.pack(side="left")
 
         # Botão "Enviar"
         self.submit_button = ttk.Button(self, text="Enviar")
         self.submit_button.configure(style='My.TButton')
         self.submit_button["command"] = self.submit
         self.submit_button.pack(side="right")
-    
+
     # Funções do botão "Enviar"
     def submit(self):
-        
+
         """Funções do botão "Enviar". Quando o usuário clicar no botão
         será enviado para o servidor e também será aberta uma nova janela
-        que retornara uma mensagem para o usuário informando que as informações 
+        que retornara uma viagem para o usuário informando que as informações
         foram enviadas com sucesso."""
 
         name = self.name_entry.get()
@@ -88,20 +116,8 @@ class application(tk.Frame):
         print("E-mail:", email)
 
         cursor = db.cursor()
-        cursor.execute(sql,(name, lastname, email))
+        cursor.execute(sql, (name, lastname, email))
         db.commit()
-        # if db.commit() is True:
-        #     dbWindow = tk.Toplevel(self.master)
-        #     dbMsg = tk.Label(dbWindow, text="Dados inseridos com sucesso!")
-        #     dbMsg.pack()
-        # else:
-        #     dbWindowError = tk.Toplevel(self.master)
-        #     dbError = tk.Label(dbWindowError, text="Houve algum problema com a inserção das informações no banco de dados!")
-        #     dbError.pack()
-        #     self.name_entry.delete(0, tk.END)
-        #     self.lastname_entry.delete(0, tk.END)
-        #     self.email_entry.delete(0, tk.END)
-
 
         # Cria uma nova janela
         success_window = tk.Toplevel(self.master)
@@ -115,7 +131,6 @@ class application(tk.Frame):
         self.lastname_entry.delete(0, tk.END)
         self.email_entry.delete(0, tk.END)
 
-
 root = tk.Tk()
-app = application(master=root)
+app = ServiceSelection(master=root)
 app.mainloop()
